a9f8d7e49e98f0591b3dbfdfd196eca8188ddf72
radhareddiapaku <radhareddipaku4c0@gmail.com>
2025-06-12 02:26:35 -0700

    Add files via upload

---
+++ memoization.java
@@ -0,0 +1,20 @@
+public class memoization
+{
+    static int c=0;
+   public static int fib(int n,int [] memo){
+       if(memo[n]!=-1) return memo[n];
+       c++;
+       if(n<=1) return n;
+       memo[n]=fib(n-1,memo)+fib(n-1,memo);
+       return memo[n];
+   }
+   public static void main(String[] args){
+       int n=4;
+       int[]memo=new int[n+1];
+       for(int i=0;i<memo.length;i++){
+           memo[i]=-1;
+       }
+        System.out.println(fib(n,memo));
+        System.out.println(c);
+   }
+}
