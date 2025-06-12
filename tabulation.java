29335ab507fbc706a27fa109492ba92dca54a7f9
radhareddiapaku <radhareddipaku4c0@gmail.com>
2025-06-12 02:26:49 -0700

    Add files via upload

---
+++ tabulation.java
@@ -0,0 +1,13 @@
+public class tabulation
+{
+   public static void main(String[] args){
+       int n=4;
+       int[]memo=new int[n+1];
+       memo[1]=1;
+       for(int i=2;i<memo.length;i++)
+       {
+           memo[i]=memo[i-1]+memo[i-2];
+       }
+       System.out.println(memo[n]);
+   }
+}
